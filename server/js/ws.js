492a8a2dfdb33d2563a15a36bcdb981d6680e1fc
Kalizorah <saberthesaber@gmail.com>
2016-06-09 10:45:33 -0500

    changed library references
    - 'path.exists()' is now 'fs.exists()'
    - all references to 'websocket-server' are now 'node-websocket-server'

--- server/js/ws.js
+++ server/js/ws.js
@@ -1,8 +1,8 @@
 
 var cls = require("./lib/class"),
     url = require('url'),
-    wsserver = require("websocket-server"),
-    miksagoConnection = require('websocket-server/lib/ws/connection'),
+    wsserver = require("node-websocket-server"),
+    miksagoConnection = require('node-websocket-server/lib/ws/connection'),
     worlizeRequest = require('websocket').request,
     http = require('http'),
     Utils = require('./utils'),
